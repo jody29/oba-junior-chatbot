cdfcfd0959377ff73c5302d9e2eb4ca8591ddd98
basv1996 <bas.vugts@gmail.com>
2022-06-09 11:17:47 +0200

    fock off

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ const path = require('path')
 const app = express()
 const server = http.createServer(app)
 
+// Gebruik template engine Express voor DYNAMISCHE content
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'src/views'))
+app.set('views', path.join(__dirname, 'src/views/Pages'))
 app.use(express.static(__dirname + '/public'))
+
+
+
+app.get('/', (req, res)=> {
+    res.render("index.ejs")
+})
+
+app.post('/', (req, res)=> {
+    console.log(req.body)
+})
+
+app.get('/homePage', (req, res)=> {
+    res.render("homePage.ejs")
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });

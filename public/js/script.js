1722da1f141efad1508adfdae9e4fb98610f983e
basv1996 <bas.vugts@gmail.com>
2022-06-13 11:19:23 +0200

    back/forward btn with little bugs in the code

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,36 @@
 const buttons = document.querySelectorAll('#controls li i')
 const slider = document.querySelector('.slider-wrapper ul:first-of-type')
+const sliderMonsters = document.querySelector("section:nth-of-type(2) .slider-wrapper ul:first-of-type")
+const sliderPrinsessen = document.querySelector("section:nth-of-type(3) .slider-wrapper ul:first-of-type")
+console.log(buttons)
 
 buttons[0].classList.add('disable')
+buttons[2].classList.add('disable')
+buttons[4].classList.add('disable')
 
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
+        // Spannend
         if (slider.scrollLeft < 1151) {
             buttons[0].classList.add('disable')
         }
+        // Monsters
+        if (sliderMonsters.scrollLeft < 1151) {
+            buttons[2].classList.add('disable')
+        }
+       //  Prinsessen
+        if (sliderPrinsessen.scrollLeft < 1151) {
+            buttons[4].classList.add('disable')
+        }
+
+        	// Spannende slider
         if (Array.prototype.indexOf.call(buttons, e.target) === 0) {
             slider.scrollLeft -= 1150
             buttons[1].classList.remove('disable')
+            
               
-        } else if (Array.prototype.indexOf.call(buttons, e.target) === 1) {
+        } 
+        else if (Array.prototype.indexOf.call(buttons, e.target) === 1) {
             slider.scrollLeft += 1150
             buttons[0].classList.remove('disable')
 
@@ -21,6 +39,37 @@ buttons.forEach(button => {
             }
         }
 
+        // Monster Slider
+        if (Array.prototype.indexOf.call(buttons, e.target) === 2) {
+            sliderMonsters.scrollLeft -= 1150
+            buttons[3].classList.remove('disable')
+            
+              
+        } 
+        else if (Array.prototype.indexOf.call(buttons, e.target) === 3) {
+            sliderMonsters.scrollLeft += 1150
+            buttons[2].classList.remove('disable')
+
+            if (sliderMonsters.scrollLeft >= 2000) {
+                buttons[3].classList.add('disable')
+            }
+        }
+
+            // // Prinsessen Slider
+            if (Array.prototype.indexOf.call(buttons, e.target) === 4) {
+                sliderPrinsessen.scrollLeft -= 1150
+                buttons[5].classList.remove('disable')
+                
+                  
+            } 
+            else if (Array.prototype.indexOf.call(buttons, e.target) === 5) {
+                sliderPrinsessen.scrollLeft += 1150
+                buttons[4].classList.remove('disable')
+    
+                if (sliderPrinsessen.scrollLeft >= 2000) {
+                    buttons[5].classList.add('disable')
+                }
+            }
     })
 })
 
